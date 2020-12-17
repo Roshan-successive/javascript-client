7426bcffeab36305b8e1a74a06d7f0faa6e186b3
Roshan chaudhary <roshan.chaudhary@tr15.successivetech.com>
2020-12-17 16:04:49 +0530

    feat:Task completed

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
-import { Error, Input } from './style'
-class TextField extends Component {
-    render() {
-        // eslint-disable-next-line react/prop-types
-        const { value, disabled, error } = this.props;
-        if (error) {
-            return (
-                <>
-                    <Input type="text" value={value} error />
-                    <Error>{error}</Error>
-                </>
-            )
-        }
-        return (
-            <Input type="text" value={value} disabled={disabled} />
-        )
-    }
-}
-export default TextField;
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Error, Input } from './style';
+
+export const TextField = (props) => {
+  const {
+    value, disabled, error, onChange, onBlur,
+  } = props;
+  // eslint-disable-next-line no-console
+
+  if (error) {
+    return (
+      <>
+        <Input type="text" value={value} error onChange={onChange} onBlur={onBlur} />
+        <Error>{error}</Error>
+      </>
+    );
+  }
+  return (
+    <Input type="text" value={value} disabled={disabled} onChange={onChange} onBlur={onBlur} />
+  );
+};
+TextField.propTypes = {
+  value: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.string.isRequired,
+};
+TextField.defaultProps = {
+  disabled: false,
+  error: '',
+  onChange: '',
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { TextField } from './TextField';
-
 export { Slider } from './Slider';
+export { SelectField } from './SelectField';
+export { RadioField } from './RadioField';
+export { ButtonField } from './Button';
