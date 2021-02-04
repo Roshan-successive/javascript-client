404b2426ce3643b65cbb220004170915dd7b1316
Roshan chaudhary <roshan.chaudhary@tr15.successivetech.com>
2021-02-04 18:22:00 +0530

    HOC, Dynamic Table Data, Add Trainee to Store

--- src/ libs/utils/api.js
+++ src/ libs/utils/api.js
@@ -1,12 +1,19 @@
+
 import axios from 'axios';
 
 const callApi = async (route, method, body) => {
   const BASE_URL = 'http://localhost:9001/api';
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: localStorage.getItem('token'),
+  };
   const response = await axios({
     method,
     url: BASE_URL + route,
     data: body,
+    headers,
   });
   return response;
 };
+
 export default callApi;
